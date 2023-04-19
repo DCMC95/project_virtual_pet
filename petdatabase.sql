ce016f4e6ff8322dbf1ffe33d122491d9ecc5661
DCMC95 <119620236+DCMC95@users.noreply.github.com>
2023-04-19 18:39:40 +0100

    Delete petdatabase.sql

--- petdatabase.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-DROP TABLE IF EXISTS petdatabase;
-
-CREATE TABLE IF NOT EXISTS Messageboard (
-    id   uuid      DEFAULT uuid_generate_v4() PRIMARY KEY,
-    time timestamp DEFAULT now(),
-    msg  text
-);
